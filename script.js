12a23779d4fd21042e2a1b5f689eeea2cc5a2025
mhmdg5673-design <mhmdg5673@gmail.com>
2026-09-14 16:56:30 +0300

    Update script.js

--- script.js
+++ script.js
@@ -5,7 +5,12 @@ function checkPassword(){
     if(pass === "Sobety_ana_w_bas"){
         document.getElementById("login").style.display = "none";
         document.getElementById("content").style.display = "block";
-        document.getElementById("bgMusic").play(); // تشغيل الأغنية تلقائياً
+        
+        // تشغيل الأغنية مع تخطي حظر الآيفون
+        let music = document.getElementById("bgMusic");
+        music.play().catch(function(error) {
+            console.log("Autoplay blocked", error);
+        });
     } else {
         alert("Wrong Password ❌ (جربي تاني يا شاطرة 😂)");
     }
